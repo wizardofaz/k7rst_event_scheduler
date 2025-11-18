02a4edcd0dfdb4cdba865ebe76461ec137470adc
Bill Bennett <bill@wizardofaz.net>
2025-11-18 05:18:34 -0700

    update version number

--- config.php
+++ config.php
@@ -15,7 +15,7 @@ require_once __DIR__ . '/csrf.php';
 
 csrf_start_session_if_needed();
 
-define('APP_VERSION', '1.0.11.15 2025-11-15');
+define('APP_VERSION', '1.0.11.17 2025-11-17');
 
 // allow setting DEBUG_LEVEL differently when 
 // running from .alpha directory
